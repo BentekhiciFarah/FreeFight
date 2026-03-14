3d38a32af7fd7b5546a719d8e05119e78a595522
Amine Chboukat <amine.chboukat@universite-paris-saclay.fr>
2026-03-14 02:47:16 +0100

    Ajouter la methode move qui permet de bouger les troupes vers une destination

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenue dans FreeFight !");
         // Lancement de l'affichage
-        List<Troupe> troupes = new ArrayList<>();
-        troupes.add(new Barbare(50, 500));
-        troupes.add(new Sorcier(120, 500));
-        troupes.add(new Pekka(190, 500));
 
-        new Affichage(troupes);
     }
         
 }
